339b8c0d248b147c8e4af6bfe82e8f1371287407
Nick Driesen <156067639+Nick3sen@users.noreply.github.com>
2025-03-22 14:24:37 +0100

    First commit

---
+++ BITL.h
@@ -0,0 +1,14 @@
+#ifndef BITL_H
+#define BITL_H
+
+#include <Arduino.h>
+
+class BITL {
+    public:
+        setup();  // Constructor
+        void sendPos();   // Initialization function
+        void sendID();  // Sample method
+    private:
+    };
+
+#endif
